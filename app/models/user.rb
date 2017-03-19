7b6c905a596c414fab56c541ef89639acbacbde3
grandtheft-spaceship <justdizon71@gmail.com>
2017-03-19 01:46:28 -0700

    :art: Sets up validations and enum in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_uniqueness_of :email, message: 'Email Has Already Being Used'
+  validates_presence_of :role
+  enum role: [:guardian, :therapist, :teacher]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170319082922) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
-    t.string   "position"
+    t.string   "position",               default: ""
     t.integer  "role"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
