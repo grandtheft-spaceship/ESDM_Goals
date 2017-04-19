54d100461acf2cb09cb2b8adb0cf1539ce4729e6
grandtheft-spaceship <justdizon71@gmail.com>
2017-04-18 18:49:06 -0700

    :art: Adds ability for teacher/therapist to be optional when creating a case

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,8 +1,8 @@
 class Case < ApplicationRecord
   belongs_to :guardian
   belongs_to :kid
-  belongs_to :therapist
-  belongs_to :teacher
+  belongs_to :therapist, optional: true
+  belongs_to :teacher, optional: true
   has_many :daily_reports
   has_many :goals
 end
