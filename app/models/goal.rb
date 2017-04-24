b0cbf772b09754ccbce30de11a33ff468f8f02e1
grandtheft-spaceship <justdizon71@gmail.com>
2017-04-23 18:16:52 -0700

    :art: Creates seed data for users

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,3 @@
 class Goal < ApplicationRecord
   belongs_to :case
-  belongs_to :kid, :through => :cases
-  belongs_to :therapist, :through => :cases
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+Kid.delete_all
+Case.delete_all
+Goal.delete_all
+Survey.delete_all
+Question.delete_all
+
+roles = ["guardian", "therapist", "teacher"]
+
+10.times do
+  User.create!({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    role: roles[rand(0..2)],
+    email: Faker::Internet.email,
+    password: "password"
+    })
+end
+
+therapist_positions = ["Pathologist", "Occupational Therapist"]
+teacher_positions = ["Phase 1", "Phase 2", "Phase 3", "Pre-K Teacher"]
+
+therapists = User.where(role: "therapist")
+teachers = User.where(role: "teacher")
+
+therapists.map { |therapist| therapist.position = therapist_positions[rand(0..1)]}
+
+therapists.each do |therapist|
+  therapist.save
+end
+
+teachers.map { |teacher| teacher.position = teacher_positions[rand(0..3)]}
+
+teachers.each do |teacher|
+  teacher.save
+end
