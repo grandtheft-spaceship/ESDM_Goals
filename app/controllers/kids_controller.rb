2caecbf41cb469e43f057727c148ffd3764d0652
grandtheft-spaceship <justdizon71@gmail.com>
2017-04-30 00:19:23 -0700

    :art: Adds show view and controller action for kids

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -4,8 +4,17 @@ class KidsController < ApplicationController
     @kids = @guardian.kids
   end
 
+  def show
+    @guardian = Guardian.find(params[:guardian_id])
+    @kid = Kid.find_by(guardian_id: @guardian.id)
+  end
+
   def new
     @guardian = Guardian.find(params[:guardian_id])
     @kid = @guardian.kids.new
   end
+
+  def create
+  end
+
 end
