23633085d37ada53b021b4f363dd4286bc7b576e
grandtheft-spaceship <justdizon71@gmail.com>
2017-05-03 04:12:44 -0700

    :art: Adds new/show actions for cases

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,2 +1,25 @@
 class CasesController < ApplicationController
+  def new
+    @case = Case.new
+  end
+
+  def create
+    @case = Case.new(case_params)
+
+    if @case.save
+      redirect_to case_path(@case)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @case = Case.find(params[:id])
+  end
+
+  private
+
+  def case_params
+    params.require(:case).permit(:guardian_id, :therapist_id, :teacher_id, :kid_id)
+  end
 end
